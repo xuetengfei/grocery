f1ae3107afc41ab6ece25e1d6858248c77ea9e4c
xuetegfei <workingxue@gmail.com>
2020-07-31 20:35:53 +0800

    data

---
+++ _git_push copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo data | md5
--- _git_push.sh
+++ _git_push.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-## chmod +x auto.sh
-
 echo "-------Auto Git Begin-------"
 git pull
 git add .
 now="$(date +"%Y-%m-%d %H:%M")"
-git commit -m "${now}"
+git commit -m data | md5
 echo "****************** commit is:$1 ${now} ***********"
 git push origin master
 echo "--------End--------"
