7945d832136443bb564e237b5237d1a138bb5c34
xuetegfei <workingxue@gmail.com>
2020-07-31 20:38:35 +0800

    date

--- _git_push.sh
+++ _git_push.sh
@@ -4,7 +4,7 @@ echo "-------Auto Git Begin-------"
 git pull
 git add .
 now="$(date +"%Y-%m-%d %H:%M")"
-git commit -m data | md5
+git commit -m  date | md5
 echo "****************** commit is:$1 ${now} ***********"
 git push origin master
 echo "--------End--------"
