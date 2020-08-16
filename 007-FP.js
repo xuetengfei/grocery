4ad1c10a0438b0d48f7d5687330fcf3754c2c71f
xuetengfei <18392015462@163.com>
2020-08-16 14:10:38 +0800

    dba2d33f088845e636c8c46175718de2

---
+++ 007-FP.js
@@ -0,0 +1,19 @@
+const { divider, log } = require('./utils');
+
+const sum = (x, y, z) => x + y + z;
+
+{
+  const curry = (fn, need = fn.length, ...args) =>
+    need <= args.length ? fn(...args) : curry.bind(null, fn, need, ...args);
+
+  log(curry(Math.min, 4)(10)(50)(2)(4)); // 2
+  log(curry(sum)(1)(2)(3)); // 4
+}
+
+divider();
+{
+  const curry = fn =>
+    fn.length === 0 ? fn() : (...args) => curry(fn.bind(null, ...args));
+
+  log(curry(sum)(1)(2)(3)); // 4
+}
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 const log = (desc, what) => {
   what ? console.log(`${desc} is:`, what) : console.log(desc);
 };
-const divider = () => console.log('====================================');
+const divider = () => console.log('====================');
 
 module.exports = {
   divider,
