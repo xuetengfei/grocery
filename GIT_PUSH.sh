29eec2605d830ba100fb05f15e13b3207ac1854c
xuetengfei <workingxue@gmail.com>
2021-03-01 12:59:47 +0800

    6ffd7b3a06db24587556baa64f05f342

--- GIT_PUSH.sh
+++ GIT_PUSH.sh
@@ -1,18 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# echo "----- Start -----"
-# find $PWD/* -empty | xargs rm
-# git pull
-# git add .
-# git commit -m "$(date | md5 )"
-# git push origin master
-# echo "----- End -----"
+## chmod +x auto.sh
 
 echo "-------Auto Git Begin-------"
-# git config pull.rebase false
-git pull --ff-only
+git config pull.rebase false
+git pull
 git add .
+now="$(date +"%Y-%m-%d %H:%M")"
 git commit -m "$(date | md5 )"
 echo "****************** commit is:$1 ${now} ***********"
-git push 
+git push origin master
 echo "--------End--------"
