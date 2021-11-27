17e7da6a3e6ccef970f4d4552923a56c17d70cbc
xuetengfei <workingxue@gmail.com>
2021-11-27 14:25:16 +0800

    2526dc8d3b730480338e514f675a02be

--- utils.js
+++ utils.js
@@ -1,8 +1,9 @@
+const divider = () => console.log('-'.repeat(20) + 'logger' + '-'.repeat(20));
+
 const log = (desc, what) => {
+  divider();
   what ? console.log(`${desc} is:`, what) : console.log(desc);
 };
-const divider = () => console.log('====================');
-
 module.exports = {
   divider,
   log,
