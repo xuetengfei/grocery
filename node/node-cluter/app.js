57bae5d7b4de1a860ab828f0bb6ead9e5c289476
xuetengfei <workingxue@gmail.com>
2022-01-16 15:47:56 +0800

    bc3d5b2485b695555a8f0da99d542a67

--- node/node-cluter/app.js
+++ node/node-cluter/app.js
@@ -8,14 +8,23 @@ const numCPUs = os.cpus().length;
 
 app.get('/', function (_req, res) {
   res.json({ success: true, pid });
+  // cluster.worker.kill();
 });
 
 if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; ++i) {
     cluster.fork();
   }
+  cluster.on('exit', function (worker, code, signal) {
+    console.log(`worker ${worker.process.pid} is died`);
+    cluster.fork();
+  });
 } else {
   app.listen(3200, () => {
     console.log(`is running on 3200,pid is ${pid}`);
   });
 }
+
+// app.listen(3200, () => {
+//   console.log(`is running on 3200,pid is ${pid}`);
+// });
