838c08faebbb01d2050ef438e402fb34268ccd36
xuetengfei <workingxue@gmail.com>
2022-01-26 11:44:48 +0800

    ef46d90d5a0d10d3bd0ddb988df34f8a

--- express/express-api/app.js
+++ express/express-api/app.js
@@ -86,10 +86,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE,
 });
 
 // connection.connect();
