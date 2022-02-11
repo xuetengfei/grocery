043f691e7d39c0413d50351c78e229971d1c2804
xuetengfei <workingxue@gmail.com>
2022-02-11 16:02:44 +0800

    database/connection

--- express/plain-sequelize/5_build_save_creat.js
+++ express/plain-sequelize/5_build_save_creat.js
@@ -1,48 +1,30 @@
 const Sequelize = require('sequelize');
-require('dotenv/config');
+const database = require('./database/connection');
 
 /* 
 使用build()和save()分两步或使用create()一步创建新行。
 */
 
-const sequelize = new Sequelize(
-  process.env.database,
-  process.env.username,
-  process.env.password,
-  {
-    dialect: 'mysql',
-    host: process.env.host,
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 30000,
-    },
-    define: {
-      // 关闭时间戳记 createdAt updatedAt
-      timestamps: false,
-    },
-  },
-);
-
-let Note = sequelize.define('notes', {
+let Note = database.define('notes', {
   description: Sequelize.STRING,
 });
 
 async function main() {
   try {
     // sqeuelize.syn()同步所有model,force选项丢弃的表，如果它的创建之前就存在。
-    // await sequelize.sync({ force: true });
+    // await database.sync({ force: true });
 
     // 1 build +save
     // const note = Note.build({ description: 'Took a cold bath' });
     // await note.save();
-
+    // console.log(note.get({ plain: true }));
     // 2 save
-    await Note.create({ description: 'xxx' });
+    const note = await Note.create({ description: 'xxx3' });
+    console.log(note.get({ plain: true }));
   } catch (error) {
     console.log('failed to create notes', error);
   } finally {
-    await sequelize.close();
+    await database.close();
   }
 }
 
