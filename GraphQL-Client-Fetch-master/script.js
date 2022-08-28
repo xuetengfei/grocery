76c989dd0d5336dc48c147d9b8d06a8862324ad5
xuetengfei <workingxue@gmail.com>
2022-08-28 12:35:53 +0800

    a10761abbf196333ab076ce3041ded1a

---
+++ GraphQL-Client-Fetch-master/script.js
@@ -0,0 +1,58 @@
+const continentSelect = document.getElementById('continent-select')
+const countryList = document.getElementById('countries-list')
+
+const API ='https://countries.trevorblades.com/'
+function queryFetch(query, variables) {
+  return fetch(API, {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: query,
+      variables: variables
+    })
+  }).then(res => res.json())
+}
+
+
+queryFetch(`
+  query {
+    continents {
+      name
+      code
+    }
+  }
+`).then(data => {
+  data.data.continents.forEach(continent => {
+    const option = document.createElement('option')
+    option.value = continent.code
+    option.innerText = continent.name
+    continentSelect.append(option)
+  })
+})
+
+continentSelect.addEventListener('change', async e => {
+  const continentCode = e.target.value
+  console.log(35,continentCode);
+  const countries = await getContinentCountries(continentCode)
+  countryList.innerHTML = ''
+  countries.forEach(country => {
+    const element = document.createElement('div')
+    element.innerText = country.name
+    countryList.append(element)
+  })
+})
+
+function getContinentCountries(continentCode) {
+  return queryFetch(`
+    query getCountries($code: ID!) {
+      continent(code: $code) {
+        countries {
+          name
+        }
+      }
+    }
+  `, { code: continentCode }).then(data => {
+    return data.data.continent.countries
+  })
+}
+
