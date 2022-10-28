7c1b5b23d74ece8be7cf9872e0dc7da5e8958dff
xuetengfei <18392015462@163.com>
2022-10-28 22:34:19 +0800

    3d346b4b973d9c8bbafed89d316777a2

---
+++ Decorators/index.ts
@@ -0,0 +1,32 @@
+#!/usr/local/bin/ts-node
+
+// [TypeScript: Documentation - Decorators](https://www.typescriptlang.org/docs/handbook/decorators.html#parameter-decorators)
+
+function first() {
+  console.log("first(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("first(): called");
+    console.log("first(): called~target",target);
+    console.log("first(): called~propertyKey",propertyKey);
+    console.log("first(): called~descriptor",descriptor);
+    console.log("first(): called~descriptor~value",descriptor.value);
+  };
+}
+
+function second() {
+  console.log("second(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("second(): called");
+  };
+}
+
+
+class C{
+    @first()
+    @second()
+    method():void{
+        console.log('c~metod')
+    }
+}
+
+new C().method()
